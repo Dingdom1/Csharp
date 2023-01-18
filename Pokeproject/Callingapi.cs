ef973a0f34dae6dd12275ebe291dcd259fd1b270
Dingdom1 <dingdom97@gmail.com>
2023-01-18 20:26:27 +1100

    Able to create Csv file with Json

--- Pokeproject/Callingapi.cs
+++ Pokeproject/Callingapi.cs
@@ -1,21 +1,27 @@
-﻿using System.Net.Http.Headers;
+﻿using System.Text.Json;
+using System.Net.Http.Headers;
+
 
 using HttpClient client = new();
+    
 client.DefaultRequestHeaders.Accept.Clear();
 client.DefaultRequestHeaders.Accept.Add(
     new MediaTypeWithQualityHeaderValue("applications/json"));
 client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
+//public class Myclass
+//public static async Task ExampleMethodAsync()
 
-await ProcessRepositoriesAsync(client);
+    await ProcessRepositoriesAsync(client);
+    
+    static async Task ProcessRepositoriesAsync(HttpClient client)
 
-static async Task ProcessRepositoriesAsync(HttpClient client)
 {
-    {
         var json = await client.GetStringAsync(
         "https://pokeapi.co/api/v2/pokedex/2"); //list all the pokedex entries of 1st gen Pokemon 1-152
-        Console.Write(json);
-    }
+        var records = JsonSerializer.Deserialize<dynamic>(json);
+        System.IO.File.WriteAllText(@"pokemon.json", json);
 }
+
 
 
